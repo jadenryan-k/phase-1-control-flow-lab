945a1ceeedac345794e69408ac35d97c1eb3db55
Jaden Ryan <jaden.kitonga@student.moringaschool.com>
2022-06-08 17:44:05 +0300

    commit tree 1e7e19daba785ef689ea0affd7876c087ae7c8b5

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+    let ride 
+    if(someValue <= 400){
+      ride = someValue
+      return 'This one is on me!'
+    }
+    else if(someValue <= 2500){
+      ride = someValue
+      return 'I will gladly take your thirty bucks.'
+    }
+    else {
+      return 'No can do.'
+    }    
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+    
 }
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous' :
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+
+      default:
+      return 'Bye.'
+  }
 }
